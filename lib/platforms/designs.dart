d56065a7f49585c76c8e30f5d54b2f7ad9314eed
Suesi Tran <tranhathuy@gmail.com>
2022-07-06 10:05:27 +1000

    Enable floating action button

--- lib/platforms/designs.dart
+++ lib/platforms/designs.dart
@@ -3,73 +3,117 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart' as a;
 
-export 'package:flutter/material.dart';
+export 'package:flutter/material.dart' hide Scaffold, ScaffoldMessenger;
 
 abstract class _PlatformScreen extends StatelessWidget {
   const _PlatformScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final a = appBarActions();
+    final a = appBarActions(context);
     final t = title(context);
 
     if (Platform.isIOS) {
-      return _buildCupertino(t, a);
+      return _buildCupertino(context, t, a);
     }
 
-    return _buildMaterial(t, a);
+    return _buildMaterial(context, t, a);
   }
 
-  Widget _buildCupertino(String? title, List<ScreenAction>? actions);
+  Widget _buildCupertino(BuildContext context, String? title, List<ScreenAction>? actions);
 
-  Widget _buildMaterial(String? title, List<ScreenAction>? actions);
+  Widget _buildMaterial(BuildContext context, String? title, List<ScreenAction>? actions);
 
-  List<ScreenAction>? appBarActions() {
+  FloatingActionButtonConfig? floatingActionButtonConfig(BuildContext context) {
+    return null;
+  }
+
+  List<ScreenAction>? appBarActions(BuildContext context) {
     return null;
   }
 
   String? title(BuildContext context) {
     return null;
   }
+
+  Alignment _toAlign(FloatingActionButtonLocation location) {
+    switch (location) {
+      case FloatingActionButtonLocation.centerDocked:
+      case FloatingActionButtonLocation.centerFloat:
+        return Alignment.bottomCenter;
+      case FloatingActionButtonLocation.centerTop:
+        return Alignment.topCenter;
+      case FloatingActionButtonLocation.endDocked:
+      case FloatingActionButtonLocation.endFloat:
+        return Alignment.bottomRight;
+      case FloatingActionButtonLocation.endTop:
+        return Alignment.topRight;
+      case FloatingActionButtonLocation.startDocked:
+      case FloatingActionButtonLocation.startFloat:
+        return Alignment.bottomLeft;
+      case FloatingActionButtonLocation.startTop:
+        return Alignment.topLeft;
+      default:
+        return Alignment.bottomCenter;
+    }
+  }
 }
 abstract class Screen extends _PlatformScreen {
   const Screen({Key? key}) : super(key: key);
 
   @override
-  Widget _buildCupertino(String? title, List<ScreenAction>? actions) {
-    return a.CupertinoPageScaffold(
-      navigationBar: title == null
-          ? null
-          : a.CupertinoNavigationBar(
-              middle: Text(title),
-              trailing: actions?.isNotEmpty == true
-                  ? Row(
-                      children:
-                          actions!.map<Widget>((e) => e._toWidget()).toList())
-                  : null,
+  Widget _buildCupertino(BuildContext context, String? title, List<ScreenAction>? actions) {
+    final FloatingActionButtonConfig? config = floatingActionButtonConfig(context);
+    final screenPading = MediaQuery.of(context).padding;
+
+    return Stack(
+      children: [
+        a.CupertinoPageScaffold(
+          navigationBar: title == null
+              ? null
+              : a.CupertinoNavigationBar(
+            middle: Text(title),
+            trailing: actions?.isNotEmpty == true
+                ? Row(
+                children:
+                actions!.map<Widget>((e) => e._toWidget()).toList())
+                : null,
+          ),
+          child: a.SafeArea(
+            child: Builder(
+              builder: (context) => body(context),
             ),
-      child: a.SafeArea(
-        child: Builder(
-          builder: (context) => body(context),
+          ),
         ),
-      ),
+        if (config != null) Container(
+          padding: EdgeInsets.only(
+              bottom: screenPading.bottom,
+          left: 25 + screenPading.left,
+          right: 25 + screenPading.right,
+          top: 25 + screenPading.top),
+          alignment: _toAlign(config.location),
+          child: config.button,)
+      ],
     );
   }
 
   @override
-  Widget _buildMaterial(String? title, List<ScreenAction>? actions) {
-    final appBarAction = appBarActions();
+  Widget _buildMaterial(BuildContext context, String? title, List<ScreenAction>? actions) {
+    final FloatingActionButtonConfig? config = floatingActionButtonConfig(context);
 
     return Scaffold(
       appBar: title == null
           ? null
           : AppBar(
               title: Text(title),
-              actions: appBarAction?.map<Widget>((e) => e._toWidget()).toList(),
+              actions: actions?.map<Widget>((e) => e._toWidget()).toList(),
             ),
       body: Builder(
         builder: (context) => body(context),
       ),
+      floatingActionButton: config?.button,
+      floatingActionButtonLocation: config?.location,
+      floatingActionButtonAnimator: config?.animator,
     );
   }
 
@@ -95,20 +139,32 @@ abstract class TabScreen extends _PlatformScreen {
   const TabScreen({Key? key}) : super(key: key);
 
   @override
-  Widget _buildCupertino(String? title, List<ScreenAction>? actions) {
+  Widget _buildCupertino(BuildContext context, String? title, List<ScreenAction>? actions) {
     final List<TabScreenContent> content = buildTabScreenContent();
+    final FloatingActionButtonConfig? config = floatingActionButtonConfig(context);
 
-    return a.CupertinoTabScaffold(
-      tabBar: a.CupertinoTabBar(
-        items: content.map((e) => e.bottomNavigationBarItem).toList(),
-      ),
-      tabBuilder: (context, index) => content[index].page,
+    return Stack(
+      children: [
+        a.CupertinoTabScaffold(
+          tabBar: a.CupertinoTabBar(
+            items: content.map((e) => e.bottomNavigationBarItem).toList(),
+          ),
+          tabBuilder: (context, index) => content[index].page,
+        ),
+        if (config != null) Align(
+          alignment: _toAlign(config.location),
+          child: Padding(
+            padding: EdgeInsets.all(25.0 + MediaQuery.of(context).padding.bottom),
+            child: config.button,
+          ),
+        )
+      ],
     );
   }
 
   @override
-  Widget _buildMaterial(String? title, List<ScreenAction>? actions) {
-    final appBarAction = appBarActions();
+  Widget _buildMaterial(BuildContext context, String? title, List<ScreenAction>? actions) {
+    final appBarAction = appBarActions(context);
     final PageController pageController = PageController();
     final List<TabScreenContent> content = buildTabScreenContent();
 
@@ -140,4 +196,14 @@ class TabScreenContent {
   final Widget page;
 
   TabScreenContent(this.bottomNavigationBarItem, this.page);
+}
+
+class FloatingActionButtonConfig {
+  final FloatingActionButton button;
+  final FloatingActionButtonLocation location;
+  final FloatingActionButtonAnimator animator;
+
+  FloatingActionButtonConfig({
+    required this.button, this.location = FloatingActionButtonLocation.centerDocked, this.animator = FloatingActionButtonAnimator.scaling
+});
 }
