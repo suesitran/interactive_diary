b340a89d673d46178e2226bdeef9c312182d74dc
Suesi Tran <tranhathuy@gmail.com>
2022-07-30 22:45:07 +1000

    clean up package to avoid showing material/cupertino widgets

--- packages/nartus_ui_package/test/app_impl_test.dart
+++ packages/nartus_ui_package/test/app_impl_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 
