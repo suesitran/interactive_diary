6c30c794edd944f0cb83b9ad515bd6a02555c505
Suesi Tran <tranhathuy@gmail.com>
2022-08-10 00:41:20 +1000

    Load Google Map with default location

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 
 class IDHome extends Screen {
@@ -5,8 +8,15 @@ class IDHome extends Screen {
 
   @override
   Widget body(BuildContext context) => Stack(
-      children: [
-
-      ],
-  );
+        children: <Widget>[
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: const GoogleMap(
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(-37.815561, 144.968915), zoom: 15),
+            ),
+          )
+        ],
+      );
 }
