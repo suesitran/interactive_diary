8819131a156b31d1e12e9e5b7d2fbbac556d0dfb
Suesi Tran <tranhathuy@gmail.com>
2022-08-10 00:39:39 +1000

    format code

--- packages/nartus_ui_package/test/app_impl_test.dart
+++ packages/nartus_ui_package/test/app_impl_test.dart
@@ -6,39 +6,39 @@ import 'package:nartus_ui_package/nartus_ui.dart';
 void main() {
   group('Test adaptive constructor', () {
     testWidgets('When platform is iOS, use CupertinoApp',
-            (WidgetTester tester) async {
-          debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+        (WidgetTester tester) async {
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
 
-          const app = App.adaptive(
-              home: Center(
-                child: Text('Hello'),
-              ));
+      const app = App.adaptive(
+          home: Center(
+        child: Text('Hello'),
+      ));
 
-          await tester.pumpWidget(app);
-          await tester.pumpAndSettle();
+      await tester.pumpWidget(app);
+      await tester.pumpAndSettle();
 
-          expect(find.byType(CupertinoApp), findsOneWidget);
-          expect(find.byType(MaterialApp), findsNothing);
+      expect(find.byType(CupertinoApp), findsOneWidget);
+      expect(find.byType(MaterialApp), findsNothing);
 
-          debugDefaultTargetPlatformOverride = null;
-        });
+      debugDefaultTargetPlatformOverride = null;
+    });
 
     testWidgets('When platform is Android, use MaterialApp',
-            (WidgetTester tester) async {
-          debugDefaultTargetPlatformOverride = TargetPlatform.android;
+        (WidgetTester tester) async {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
 
-          const app = App.adaptive(
-              home: Center(
-                child: Text('Hello'),
-              ));
+      const app = App.adaptive(
+          home: Center(
+        child: Text('Hello'),
+      ));
 
-          await tester.pumpWidget(app);
-          await tester.pumpAndSettle();
+      await tester.pumpWidget(app);
+      await tester.pumpAndSettle();
 
-          expect(find.byType(MaterialApp), findsOneWidget);
-          expect(find.byType(CupertinoApp), findsNothing);
+      expect(find.byType(MaterialApp), findsOneWidget);
+      expect(find.byType(CupertinoApp), findsNothing);
 
-          debugDefaultTargetPlatformOverride = null;
-        });
+      debugDefaultTargetPlatformOverride = null;
+    });
   });
 }
