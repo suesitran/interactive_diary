7d6d91d836110bb440db9940f34d3d7c2f41c81b
Suesi Tran <tranhathuy@gmail.com>
2022-08-09 21:07:17 +1000

    Add a sample home screen

---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:nartus_ui_package/nartus_ui.dart';
+
+class IDHome extends Screen {
+  const IDHome({Key? key}) : super(key: key);
+
+  @override
+  Widget body(BuildContext context) => Stack(
+      children: [
+
+      ],
+  );
+}
