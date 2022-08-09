45f48cdb728e83df1edcfac0c904dd4796b4fe2d
Suesi Tran <tranhathuy@gmail.com>
2022-08-09 20:59:53 +1000

    Change to use adaptive constructor

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:nartus_ui_package/nartus_ui.dart';
 import 'package:interactive_diary/features/signin/sign_in_screen.dart';
 import 'package:interactive_diary/route/map_route.dart' as routes;
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:interactive_diary/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +10,10 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(App(
+  runApp(App.adaptive(
     home: const SignInScreen(),
     title: 'Interactive Diary',
-    theme: Theme(
-      primaryColor: Colors.deepOrange
-    ),
+    theme: Theme(primaryColor: Colors.deepOrange),
     routes: routes.appRoute,
   ));
 }
--- packages/nartus_ui_package/lib/app.dart
+++ packages/nartus_ui_package/lib/app.dart
@@ -5,55 +5,126 @@ import 'package:flutter/material.dart' show MaterialApp, ThemeData, Brightness;
 
 import 'theme.dart';
 
-class App extends StatelessWidget {
-  final String title;
-  final Theme? theme;
-  final Theme? darkTheme;
-  final Map<String, WidgetBuilder>? routes;
-  final Widget home;
+enum _AppType { material, cupertino, adaptive }
 
-  const App({
-    Key? key,
-    required this.home,
-    this.title = '',
-    this.theme,
-    this.darkTheme,
-    this.routes,
-  }) : super(key: key);
+class App extends StatelessWidget {
+  final String _title;
+  final Theme? _theme;
+  final Theme? _darkTheme;
+  final Map<String, WidgetBuilder>? _routes;
+  final Widget _home;
+
+  final _AppType _appType;
+
+  const App._(
+      {Key? key,
+      required Widget home,
+      String title = '',
+      Theme? theme,
+      Theme? darkTheme,
+      Map<String, WidgetBuilder>? routes,
+      required _AppType appType})
+      : _home = home,
+        _title = title,
+        _theme = theme,
+        _darkTheme = darkTheme,
+        _routes = routes,
+        _appType = appType,
+        super(key: key);
+
+  const App.material(
+      {Key? key,
+      required Widget home,
+      String title = '',
+      Theme? theme,
+      Theme? darkTheme,
+      Map<String, WidgetBuilder>? routes})
+      : this._(
+            key: key,
+            home: home,
+            title: title,
+            theme: theme,
+            darkTheme: darkTheme,
+            routes: routes,
+            appType: _AppType.material);
+
+  const App.cupertino(
+      {Key? key,
+      required Widget home,
+      String title = '',
+      Theme? theme,
+      Theme? darkTheme,
+      Map<String, WidgetBuilder>? routes})
+      : this._(
+            key: key,
+            home: home,
+            title: title,
+            theme: theme,
+            darkTheme: darkTheme,
+            routes: routes,
+            appType: _AppType.cupertino);
+
+  const App.adaptive(
+      {Key? key,
+      required Widget home,
+      String title = '',
+      Theme? theme,
+      Theme? darkTheme,
+      Map<String, WidgetBuilder>? routes})
+      : this._(
+            key: key,
+            home: home,
+            title: title,
+            theme: theme,
+            darkTheme: darkTheme,
+            routes: routes,
+            appType: _AppType.adaptive);
 
   @override
   Widget build(BuildContext context) {
-    if (defaultTargetPlatform == TargetPlatform.iOS) {
-      return CupertinoApp(
-        title: title,
-        theme: _convertToCupertinoThemeData(theme, darkTheme),
-        routes: routes ?? {},
-        home: home,
-      );
+    switch (_appType) {
+      case _AppType.material:
+        return _buildMaterialApp();
+      case _AppType.cupertino:
+        return _buildCupertinoApp();
+      case _AppType.adaptive:
+        return defaultTargetPlatform == TargetPlatform.iOS
+            ? _buildCupertinoApp()
+            : _buildMaterialApp();
     }
-
-    return MaterialApp(
-      title: title,
-      theme: _convertToMaterialThemeData(theme),
-      darkTheme: _convertToMaterialThemeData(darkTheme),
-      routes: routes ?? {},
-      home: home,
-    );
   }
 
-  CupertinoThemeData _convertToCupertinoThemeData(Theme? theme, Theme? darkTheme) {
-    final brightness = MediaQueryData.fromWindow(WidgetsBinding.instance.window).platformBrightness;
+  CupertinoApp _buildCupertinoApp() => CupertinoApp(
+        title: _title,
+        theme: _convertToCupertinoThemeData(_theme, _darkTheme),
+        routes: _routes ?? {},
+        home: _home,
+      );
+
+  MaterialApp _buildMaterialApp() => MaterialApp(
+        title: _title,
+        theme: _convertToMaterialThemeData(_theme),
+        darkTheme: _convertToMaterialThemeData(_darkTheme),
+        routes: _routes ?? {},
+        home: _home,
+      );
+
+  CupertinoThemeData _convertToCupertinoThemeData(
+      Theme? theme, Theme? darkTheme) {
+    final brightness = MediaQueryData.fromWindow(WidgetsBinding.instance.window)
+        .platformBrightness;
 
     return CupertinoThemeData(
-      brightness: brightness == Brightness.dark ? darkTheme?.brightness : theme?.brightness,
-      primaryColor: brightness == Brightness.dark ? darkTheme?.primaryColor : theme?.primaryColor
-    );
+        brightness: brightness == Brightness.dark
+            ? darkTheme?.brightness
+            : theme?.brightness,
+        primaryColor: brightness == Brightness.dark
+            ? darkTheme?.primaryColor
+            : theme?.primaryColor);
   }
 
   ThemeData _convertToMaterialThemeData(Theme? theme) {
     return ThemeData(
-      brightness: theme?.brightness,
-      primaryColor: theme?.primaryColor
-    );
+        brightness: theme?.brightness, primaryColor: theme?.primaryColor);
   }
 }
