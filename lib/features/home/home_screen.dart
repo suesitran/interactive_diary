e2d132e0f7ebdebbfdf624397f858f34d4dd9773
Suesi Tran <tranhathuy@gmail.com>
2022-08-10 11:25:59 +1000

    Remove unnecessary SizedBox

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 
@@ -8,14 +6,10 @@ class IDHome extends Screen {
 
   @override
   Widget body(BuildContext context) => Stack(
-        children: <Widget>[
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: const GoogleMap(
-              initialCameraPosition: CameraPosition(
-                  target: LatLng(-37.815561, 144.968915), zoom: 15),
-            ),
+        children: const <Widget>[
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+                target: LatLng(-37.815561, 144.968915), zoom: 15),
           )
         ],
       );
