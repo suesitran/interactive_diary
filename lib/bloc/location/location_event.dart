f89b1a0a2cf5277f57a8c1cf8565d3343518842c
Suesi Tran <tranhathuy@gmail.com>
2022-08-11 21:02:45 +1000

    add event to request location service

--- lib/bloc/location/location_event.dart
+++ lib/bloc/location/location_event.dart
@@ -3,3 +3,5 @@ part of 'location_bloc.dart';
 abstract class LocationEvent {}
 
 class RequestCurrentLocationEvent extends LocationEvent {}
+
+class RequestLocationServiceEvent extends LocationEvent {}
