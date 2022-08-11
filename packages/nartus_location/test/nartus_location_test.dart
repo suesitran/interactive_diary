303c1ff9923131cef6b2ac1f37aceba0a73557ec
Suesi Tran <tranhathuy@gmail.com>
2022-08-11 22:29:08 +1000

    Add tests

--- packages/nartus_location/test/nartus_location_test.dart
+++ packages/nartus_location/test/nartus_location_test.dart
@@ -23,42 +23,60 @@ void main() {
         throwsA(isA<LocationServiceDisableException>()));
   });
 
-  test('given location permission is denied, when get current location, then throw LocationPermissionNotGrantedException',() async {
+  test(
+      'given location permission is denied, when get current location, then throw LocationPermissionNotGrantedException',
+      () async {
     // given
-    when(location.serviceEnabled()).thenAnswer((realInvocation) => Future.value(true));
-    when(location.hasPermission()).thenAnswer((realInvocation) => Future.value(PermissionStatus.denied));
+    when(location.serviceEnabled())
+        .thenAnswer((realInvocation) => Future.value(true));
+    when(location.hasPermission())
+        .thenAnswer((realInvocation) => Future.value(PermissionStatus.denied));
 
     // then
-    expect(() => service.getCurrentLocation(), throwsA(isA<LocationPermissionNotGrantedException>()));
+    expect(() => service.getCurrentLocation(),
+        throwsA(isA<LocationPermissionNotGrantedException>()));
   });
 
-  test('give location permission is denied forever, when get current location, then throw LocationPermissionNotGrantedException', () async {
+  test(
+      'give location permission is denied forever, when get current location, then throw LocationPermissionNotGrantedException',
+      () async {
     // given
-    when(location.serviceEnabled()).thenAnswer((realInvocation) => Future.value(true));
-    when(location.hasPermission()).thenAnswer((realInvocation) => Future.value(PermissionStatus.deniedForever));
+    when(location.serviceEnabled())
+        .thenAnswer((realInvocation) => Future.value(true));
+    when(location.hasPermission()).thenAnswer(
+        (realInvocation) => Future.value(PermissionStatus.deniedForever));
 
     // then
-    expect(() => service.getCurrentLocation(), throwsA(isA<LocationPermissionNotGrantedException>()));
+    expect(() => service.getCurrentLocation(),
+        throwsA(isA<LocationPermissionNotGrantedException>()));
   });
 
-  test('given location data returns null latitude and longitude, when get current location, then throw LocationDataCorruptedException', () async {
+  test(
+      'given location data returns null latitude and longitude, when get current location, then throw LocationDataCorruptedException',
+      () async {
     // given
-    when(location.serviceEnabled()).thenAnswer((realInvocation) => Future.value(true));
-    when(location.hasPermission()).thenAnswer((realInvocation) => Future.value(PermissionStatus.granted));
-    when(location.getLocation()).thenAnswer((realInvocation) => Future.value(LocationData.fromMap({})));
+    when(location.serviceEnabled())
+        .thenAnswer((realInvocation) => Future.value(true));
+    when(location.hasPermission())
+        .thenAnswer((realInvocation) => Future.value(PermissionStatus.granted));
+    when(location.getLocation())
+        .thenAnswer((realInvocation) => Future.value(LocationData.fromMap({})));
 
     // then
-    expect(() => service.getCurrentLocation(), throwsA(isA<LocationDataCorruptedException>()));
+    expect(() => service.getCurrentLocation(),
+        throwsA(isA<LocationDataCorruptedException>()));
   });
 
-  test('given location data returns valid latitude and longitude, when get current location, then return location details with lat and long', () async {
+  test(
+      'given location data returns valid latitude and longitude, when get current location, then return location details with lat and long',
+      () async {
     // given
-    when(location.serviceEnabled()).thenAnswer((realInvocation) => Future.value(true));
-    when(location.hasPermission()).thenAnswer((realInvocation) => Future.value(PermissionStatus.granted));
-    when(location.getLocation()).thenAnswer((realInvocation) => Future.value(LocationData.fromMap({
-      'latitude': 100.00,
-      'longitude': 200.00
-    })));
+    when(location.serviceEnabled())
+        .thenAnswer((realInvocation) => Future.value(true));
+    when(location.hasPermission())
+        .thenAnswer((realInvocation) => Future.value(PermissionStatus.granted));
+    when(location.getLocation()).thenAnswer((realInvocation) => Future.value(
+        LocationData.fromMap({'latitude': 100.00, 'longitude': 200.00})));
 
     // when
     LocationDetails result = await service.getCurrentLocation();
