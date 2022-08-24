04a86638006491b7b3fa6d91d622c807789d8008
Suesi Tran <tranhathuy@gmail.com>
2022-08-24 22:10:03 +1000

    Add more test

--- packages/nartus_ui_package/test/extensions/build_context_extension_test.dart
+++ packages/nartus_ui_package/test/extensions/build_context_extension_test.dart
@@ -31,6 +31,10 @@ void main() {
     expect(find.byType(CupertinoAlertDialog), findsOneWidget);
 
     // test action
+    CupertinoAlertDialog cupertinoAlertDialog = widgetTester.widget(find.byType(CupertinoAlertDialog));
+
+    expect(cupertinoAlertDialog.actions.length, 1);
+
     await widgetTester.tap(find.text('Action'));
 
     expect(counter, 1);
@@ -62,10 +66,70 @@ void main() {
     expect(find.byType(AlertDialog), findsOneWidget);
 
     // test action
+    AlertDialog alertDialog = widgetTester.widget(find.byType(AlertDialog));
+
+    expect(alertDialog.actions?.length, 1);
+
     await widgetTester.tap(find.text('Action'));
 
     expect(counter, 1);
 
     debugDefaultTargetPlatformOverride = null;
   });
+
+  testWidgets(
+      'given platform is iOS, when showDialogAdaptive without actions, then show CupertinoAlertDialog without actions',
+  (widgetTester) async {
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
+    Widget builder = Builder(
+      builder: (BuildContext context) {
+        context.showDialogAdaptive(
+            title: const Text('Title'),
+            content: const Center(
+              child: Text('Content'),
+            ),);
+        return Container();
+      },
+    );
+
+    await widgetTester.wrapCupertinoAndPump(builder);
+
+    expect(find.byType(CupertinoAlertDialog), findsOneWidget);
+
+    // test action
+    CupertinoAlertDialog cupertinoAlertDialog = widgetTester.widget(find.byType(CupertinoAlertDialog));
+
+    expect(cupertinoAlertDialog.actions.length, 0);
+
+    debugDefaultTargetPlatformOverride = null;
+  });
+
+  testWidgets(
+      'given platform is Android, when showDialogAdaptive without actions, then show MaterialAlertDialog without actions',
+          (widgetTester) async {
+        debugDefaultTargetPlatformOverride = TargetPlatform.android;
+
+        Widget builder = Builder(
+          builder: (BuildContext context) {
+            context.showDialogAdaptive(
+              title: const Text('Title'),
+              content: const Center(
+                child: Text('Content'),
+              ),);
+            return Container();
+          },
+        );
+
+        await widgetTester.wrapMaterialAndPump(builder);
+
+        expect(find.byType(AlertDialog), findsOneWidget);
+
+        // test action
+        AlertDialog alertDialog = widgetTester.widget(find.byType(AlertDialog));
+
+        expect(alertDialog.actions?.length, 0);
+
+        debugDefaultTargetPlatformOverride = null;
+      });
 }
