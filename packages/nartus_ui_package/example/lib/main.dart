3d6dbc0574b66c658e27058304c09cb218e41e78
Suesi Tran <tranhathuy@gmail.com>
2022-08-25 17:07:19 +1000

    Widget catalog

---
+++ packages/nartus_ui_package/example/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:example/screens/text_and_error_label_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:nartus_ui_package/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Widget catalog'),
+        ),
+        body: const WidgetCatalog(),
+      ),
+    );
+  }
+}
+
+class WidgetCatalog extends StatelessWidget {
+  const WidgetCatalog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ListView(
+        children: [
+          _buildWidgetTile(
+              'Text and error label', const TextAndErrorLabelScreen()),
+        ],
+      );
+
+  Widget _buildWidgetTile(String title, Widget catalogScreen) => Builder(
+      builder: (context) => ListTile(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).dividerColor,
+            width: 1.0
+          )
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 4.0
+        ),
+          trailing: const Icon(Icons.arrow_right),
+          onTap: () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => catalogScreen)),
+          title: Text(title)));
+}
---
+++ packages/nartus_ui_package/example/lib/screens/text_and_error_label_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TextAndErrorLabelScreen extends StatelessWidget {
+  const TextAndErrorLabelScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text and Error Label demo'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Text('T.B.D'),
+      ),
+    );
+  }
+}
