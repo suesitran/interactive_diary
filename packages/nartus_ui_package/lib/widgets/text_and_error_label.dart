5c0bb4fae40455cce2c5efa02246c0cf672224ae
Suesi Tran <tranhathuy@gmail.com>
2022-08-25 21:52:48 +1000

    add animation, with hardcoded height value

--- packages/nartus_ui_package/lib/widgets/text_and_error_label.dart
+++ packages/nartus_ui_package/lib/widgets/text_and_error_label.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
 
-class TextAndErrorLabel extends StatelessWidget {
+class TextAndErrorLabel extends StatefulWidget {
   final String _label;
-  final String? _error;
+  final String _error;
+  final bool _showError;
 
-  const TextAndErrorLabel({Key? key, required String label, String? error})
+  const TextAndErrorLabel({Key? key, required String label, required String error, bool showError = false})
       : _label = label,
         _error = error,
+        _showError = showError,
         super(key: key);
 
   @override
+  State<TextAndErrorLabel> createState() => _TextAndErrorLabelState();
+}
+
+class _TextAndErrorLabelState extends State<TextAndErrorLabel> with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  final Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
+  late Animation<double> _heightFactor;
+
+  final Duration _kExpand = const Duration(seconds: 5);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(duration: _kExpand, vsync: this);
+
+    _heightFactor = _controller.drive(_easeInTween);
+
+    if (widget._showError) {
+      _controller.value = 1;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      if (widget._showError) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
     return Card(
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(23)),
@@ -22,31 +63,41 @@ class TextAndErrorLabel extends StatelessWidget {
             color: Theme.of(context).colorScheme.error,
             borderRadius: const BorderRadius.all(Radius.circular(23))),
         alignment: Alignment.center,
-        child: Column(
+        child: Stack(
           children: [
-            Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.onBackground,
-                  borderRadius: const BorderRadius.all(Radius.circular(23))),
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 15.0),
-              child: Text(_label, style: Theme.of(context).textTheme.headline6),
-            ),
-            if (_error != null)
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                child: Text(
-                  _error!,
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1
-                      ?.copyWith(color: Theme.of(context).colorScheme.onError),
-                ),
-              )
+            _errorWidget(),
+            _labelWidget(),
           ],
         ),
       ),
     );
   }
+
+  Widget _labelWidget() => Builder(builder: (context) => Container(
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.onBackground,
+        borderRadius: const BorderRadius.all(Radius.circular(23))),
+    padding:
+    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 15.0),
+    child: Text(widget._label, style: Theme.of(context).textTheme.headline6),
+  ),);
+
+  Widget _errorWidget() => AnimatedBuilder(
+    animation: _controller.view,
+    builder: (context, child) {
+      return Container(
+        height: 80 * _heightFactor.value,
+          alignment: Alignment.bottomCenter,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          child: Text(
+            widget._error,
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                ?.copyWith(color: Theme.of(context).colorScheme.onError),
+          ),
+      );
+    },
+  );
 }
