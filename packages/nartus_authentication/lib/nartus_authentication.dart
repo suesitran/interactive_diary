ae7220e1d483a04235d6e0a566d417d35ea13935
DongNguyenQ <41529808+DongNguyenQ@users.noreply.github.com>
2022-08-30 15:48:20 +0700

    Added authentication service google, ignore lcov file

---
+++ packages/nartus_authentication/lib/nartus_authentication.dart
@@ -0,0 +1,3 @@
+export 'src/authentication_service.dart';
+export 'src/user_data.dart';
+export 'src/authentication_exception.dart';
---
+++ packages/nartus_authentication/lib/src/authentication_service.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nartus_authentication/src/authentication_exception.dart';
+import 'package:nartus_authentication/src/common.dart';
+
+import 'user_data.dart';
+
+class AuthenticationService {
+  final GoogleSignIn _googleSignIn;
+  final FirebaseAuth _firebaseAuth;
+
+  AuthenticationService(
+      {GoogleSignIn? googleSignIn, FirebaseAuth? firebaseAuth})
+      : _googleSignIn = googleSignIn ?? GoogleSignIn(),
+        _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  Future<UserDetail> signinGoogle() async {
+    GoogleSignInAccount? googleUser;
+    try {
+      googleUser = await _googleSignIn.signIn();
+    } on PlatformException catch (e) {
+      throw AuthUtils.convertAuthException(e.code, '${e.message} | ${e.details}');
+    }
+
+    if (googleUser == null) throw AuthenticateFailedException.userCancelled();
+
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final OAuthCredential oAuthCredential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    await _signInFirebase(oAuthCredential);
+
+    return await getCurrentUser();
+  }
+
+  Future<UserDetail> getCurrentUser() async {
+    final User? firebaseUser = _firebaseAuth.currentUser;
+    if (firebaseUser == null) throw AuthenticateFailedException.userNotFound();
+
+    return UserDetail(
+      name: firebaseUser.displayName,
+      avatarUrl: firebaseUser.photoURL,
+      phone: firebaseUser.phoneNumber,
+      email: firebaseUser.email);
+  }
+
+  Future<void> _signInFirebase(AuthCredential credential) async {
+    try {
+      await _firebaseAuth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      throw AuthUtils.convertAuthException(e.code, e.message ?? DefaultError.kErrUnknown);
+    } catch (_) {
+      throw AuthenticateFailedException.unknown();
+    }
+  }
+
+  Future signOut() async {
+    await _googleSignIn.disconnect();
+    await _googleSignIn.signOut();
+    _firebaseAuth.signOut();
+  }
+}
