c7d64a63cebf22790ca30705fdd6c89c48ea1718
Suesi Tran <tranhathuy@gmail.com>
2022-09-04 00:42:52 +1000

    Change to use Column instead

--- packages/nartus_ui_package/lib/widgets/text_error_label/text_and_error_label.dart
+++ packages/nartus_ui_package/lib/widgets/text_error_label/text_and_error_label.dart
@@ -19,24 +19,14 @@ class TextAndErrorLabel extends StatefulWidget {
 
 class _TextAndErrorLabelState extends State<TextAndErrorLabel>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  final Animatable<double> _easeInTween =
-      CurveTween(curve: Curves.fastOutSlowIn);
-  late Animation<double> _heightFactor;
-
-  final Duration _kExpand = const Duration(milliseconds: 300);
-
-  final GlobalKey _labelKey = GlobalKey();
-
-  double height = 0;
+  late final AnimationController _controller = AnimationController(
+      vsync: this, duration: const Duration(milliseconds: 300));
+  late final Animation<double> _sizeAnimation =
+      CurvedAnimation(curve: Curves.fastOutSlowIn, parent: _controller);
 
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(duration: _kExpand, vsync: this);
-
-    _heightFactor = _controller.drive(_easeInTween);
 
     if (widget._showError) {
       _controller.value = 1;
@@ -52,39 +42,31 @@ class _TextAndErrorLabelState extends State<TextAndErrorLabel>
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      height = _calculateHeightOfError();
-      if (widget._showError) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
-    });
+    if (widget._showError) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+
     return Card(
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(NartusDimens.padding24)),
       ),
-      color: Theme.of(context).backgroundColor,
+      color: Theme.of(context).colorScheme.error,
       elevation: NartusDimens.padding4,
-      child: Container(
-        decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.error,
-            borderRadius: const BorderRadius.all(
-                Radius.circular(NartusDimens.padding24))),
-        alignment: Alignment.center,
-        child: Stack(
-          children: [
-            _errorWidget(),
-            _labelWidget(),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _labelWidget(),
+          _errorWidget(),
+        ],
       ),
     );
   }
 
   Widget _labelWidget() => Builder(
         builder: (context) => Container(
-          key: _labelKey,
           alignment: Alignment.center,
           decoration: BoxDecoration(
               color: Theme.of(context).colorScheme.onBackground,
@@ -98,45 +80,22 @@ class _TextAndErrorLabelState extends State<TextAndErrorLabel>
         ),
       );
 
-  Widget _errorWidget() => AnimatedBuilder(
-        animation: _controller.view,
-        builder: (context, child) {
-          return Container(
-            height: height * _heightFactor.value,
-            alignment: Alignment.bottomCenter,
-            padding: const EdgeInsets.symmetric(
-                horizontal: NartusDimens.padding16,
-                vertical: NartusDimens.padding4),
-            child: Text(
-              widget._error,
-              style: Theme.of(context)
-                  .textTheme
-                  .subtitle1
-                  ?.copyWith(color: Theme.of(context).colorScheme.onError),
-            ),
-          );
-        },
+  Widget _errorWidget() => SizeTransition(
+        sizeFactor: _sizeAnimation,
+        axisAlignment: 1,
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          padding: const EdgeInsets.symmetric(
+              horizontal: NartusDimens.padding16,
+              vertical: NartusDimens.padding4),
+          child: Text(
+            widget._error,
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                ?.copyWith(color: Theme.of(context).colorScheme.onError),
+            textAlign: TextAlign.center,
+          ),
+        ),
       );
-
-  double _calculateHeightOfError() {
-    // height of error = height of error text + padding top + padding bottom + 1/2 label height
-    // label height
-    final double labelHeight = _labelKey.currentContext?.size?.height ?? 0.0;
-    final double labelWidth = _labelKey.currentContext?.size?.width ?? 0.0;
-
-    final textScaleFactor = MediaQuery.of(context).textScaleFactor;
-    final double paddingTop = NartusDimens.padding4 * textScaleFactor;
-    final double paddingBottom = NartusDimens.padding4 * textScaleFactor;
-
-    TextPainter painter = TextPainter()
-      ..textDirection = TextDirection.ltr
-      ..textScaleFactor = textScaleFactor
-      ..text = TextSpan(
-          text: widget._error, style: Theme.of(context).textTheme.subtitle1);
-
-    painter.layout(maxWidth: labelWidth);
-    double errorTextHeight = painter.height;
-
-    return errorTextHeight + labelHeight + (paddingTop + paddingBottom) * textScaleFactor;
-  }
 }
