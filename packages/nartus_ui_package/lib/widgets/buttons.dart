75a25a3c4b9cd6e4890c2235a35e514cbe9acb38
Suesi Tran <tranhathuy@gmail.com>
2022-09-07 10:12:38 +1000

    avoid compile error in flutter 3.0.5

--- packages/nartus_ui_package/lib/widgets/buttons.dart
+++ packages/nartus_ui_package/lib/widgets/buttons.dart
@@ -21,7 +21,8 @@ class IDButton extends StatelessWidget {
           maximumSize: const Size(double.infinity, 46),
           minimumSize: const Size(200, 46),
           elevation: 1,
-          foregroundColor: Colors.black),
+          // foregroundColor: Colors.black
+      ),
     );
   }
 }
