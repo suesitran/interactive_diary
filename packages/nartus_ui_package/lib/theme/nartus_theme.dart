a51bcf4775cff92b4603596d4ca2a5ff73ae9586
Suesi Tran <tranhathuy@gmail.com>
2022-09-08 11:46:15 +1000

    format code

--- packages/nartus_ui_package/lib/theme/nartus_theme.dart
+++ packages/nartus_ui_package/lib/theme/nartus_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'nartus_color.dart';
 
 ThemeData lightTheme = ThemeData(
-    primarySwatch: lightColorSwatch,
-    errorColor: NartusColor.errorColor,
-    materialTapTargetSize: MaterialTapTargetSize.padded,
+  primarySwatch: lightColorSwatch,
+  errorColor: NartusColor.errorColor,
+  materialTapTargetSize: MaterialTapTargetSize.padded,
 );
