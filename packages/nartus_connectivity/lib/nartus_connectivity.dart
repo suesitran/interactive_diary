875ec9666512fa5037ed8863be545374108a4817
Suesi Tran <tranhathuy@gmail.com>
2022-09-18 19:38:50 +1000

    Create connectivity service

--- packages/nartus_connectivity/lib/nartus_connectivity.dart
+++ packages/nartus_connectivity/lib/nartus_connectivity.dart
@@ -1,7 +1,39 @@
-library nartus_connectivity;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ConnectivityService {
+  /// Public accessible methods
+  Future<bool> get isConnected => _checkConnectivity();
+
+  Stream<bool> get onConnectivityChange => _connectivityChange.stream;
+
+  /// private handlers
+  final StreamController<bool> _connectivityChange = StreamController();
+
+  late final Connectivity _connectivity;
+
+  bool _currentConnectivity = false;
+
+  ConnectivityService({Connectivity? connectivity}) {
+    _connectivity = connectivity ?? Connectivity();
+
+    _checkConnectivity().then((value) => _currentConnectivity = value);
+
+    _connectivity.onConnectivityChanged.listen(_handleConnectivityChange);
+  }
+
+  void _handleConnectivityChange(ConnectivityResult result) async {
+    final updatedConnectivity = await _checkConnectivity(connectivityResult: result);
+    if (updatedConnectivity != _currentConnectivity) {
+      _currentConnectivity = updatedConnectivity;
+      _connectivityChange.add(_currentConnectivity);
+    }
+  }
+
+  Future<bool> _checkConnectivity({ConnectivityResult? connectivityResult}) async {
+    final result = connectivityResult ?? await _connectivity.checkConnectivity();
+
+    return result != ConnectivityResult.none;
+  }
 }
--- packages/nartus_connectivity/test/nartus_connectivity_test.dart
+++ packages/nartus_connectivity/test/nartus_connectivity_test.dart
@@ -1,12 +1,140 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:nartus_connectivity/nartus_connectivity.dart';
 
+import 'nartus_connectivity_test.mocks.dart';
+
+@GenerateMocks([Connectivity])
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  final Connectivity connectivity = MockConnectivity();
+
+  test('given current connectivity is none, when get isConnected, then return false', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.none));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    final result = await service.isConnected;
+
+    expect(result, false);
+  });
+
+  test('given current connectivity is bluetooth, when get isConnected, then return true', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.bluetooth));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    final result = await service.isConnected;
+
+    expect(result, true);
+  });
+
+  test('given current connectivity is wifi, when get isConnected, then return true', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.wifi));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    final result = await service.isConnected;
+
+    expect(result, true);
+  });
+
+  test('given current connectivity is ethernet, when get isConnected, then return true', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.ethernet));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    final result = await service.isConnected;
+
+    expect(result, true);
+  });
+
+  test('given current connectivity is mobile, when get isConnected, then return true', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.mobile));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    final result = await service.isConnected;
+
+    expect(result, true);
+  });
+
+  test('given current connectivity is none, when connectivity change to bluetooth, then emit true to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.none));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.bluetooth));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([true]));
+  });
+
+  test('given current connectivity is none, when connectivity change to wifi, then emit true to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.none));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.wifi));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([true]));
+  });
+
+  test('given current connectivity is none, when connectivity change to ethernet, then emit true to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.none));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.ethernet));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([true]));
+  });
+
+  test('given current connectivity is none, when connectivity change to mobile, then emit true to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.none));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.mobile));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([true]));
+  });
+
+  test('given current connectivity is bluetooth, when connectivity change to none, then emit false to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.bluetooth));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([false]));
+  });
+
+  test('given current connectivity is wifi, when connectivity change to none, then emit false to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.wifi));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([false]));
+  });
+
+  test('given current connectivity is ethernet, when connectivity change to none, then emit false to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.ethernet));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([false]));
+  });
+
+  test('given current connectivity is mobile, when connectivity change to none, then emit false to connectivity change stream', () async {
+    when(connectivity.checkConnectivity()).thenAnswer((realInvocation) => Future.value(ConnectivityResult.mobile));
+    when(connectivity.onConnectivityChanged).thenAnswer((realInvocation) => Stream.value(ConnectivityResult.none));
+
+    final ConnectivityService service = ConnectivityService(connectivity: connectivity);
+
+    expect(service.onConnectivityChange, emitsInOrder([false]));
   });
 }
