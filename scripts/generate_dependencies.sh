3a147afb6a60211bbed8814c460dcba951186a5f
Suesi Tran <tranhathuy@gmail.com>
2022-09-21 10:10:57 +1000

    update depependencies

--- scripts/generate_dependencies.sh
+++ scripts/generate_dependencies.sh
@@ -2,13 +2,12 @@
 
 flutter pub get
 flutter pub run build_runner build
-flutter --no-color pub global run intl_utils:generate
+flutter --no-color pub run intl_utils:generate
 
 for d in $(ls packages)
 	do cd packages/$d
 	flutter pub get
 	flutter pub run build_runner build
-	flutter --no-color pub global run intl_utils:generate
 	cd -
 done
 
