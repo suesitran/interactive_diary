ca06cf7e8739d188812f24a2f81b2ca6f2cab97d
Suesi Tran <tranhathuy@gmail.com>
2022-10-07 23:01:36 +1100

    Optimize nartus connectivity

--- packages/nartus_connectivity/lib/nartus_connectivity.dart
+++ packages/nartus_connectivity/lib/nartus_connectivity.dart
@@ -1,42 +1,8 @@
 import 'dart:async';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-class ConnectivityService {
+abstract class ConnectivityService {
   /// Public accessible methods
-  Future<bool> get isConnected => _checkConnectivity();
+  Future<bool> get isConnected;
 
-  Stream<bool> get onConnectivityChange => _connectivityChange.stream;
-
-  /// private handlers
-  final StreamController<bool> _connectivityChange = StreamController();
-
-  late final Connectivity _connectivity;
-
-  bool _currentConnectivity = false;
-
-  ConnectivityService({Connectivity? connectivity}) {
-    _connectivity = connectivity ?? Connectivity();
-
-    _checkConnectivity().then((value) => _currentConnectivity = value);
-
-    _connectivity.onConnectivityChanged.listen(_handleConnectivityChange);
-  }
-
-  void _handleConnectivityChange(ConnectivityResult result) async {
-    final updatedConnectivity =
-        await _checkConnectivity(connectivityResult: result);
-    if (updatedConnectivity != _currentConnectivity) {
-      _currentConnectivity = updatedConnectivity;
-      _connectivityChange.add(_currentConnectivity);
-    }
-  }
-
-  Future<bool> _checkConnectivity(
-      {ConnectivityResult? connectivityResult}) async {
-    final result =
-        connectivityResult ?? await _connectivity.checkConnectivity();
-
-    return result != ConnectivityResult.none;
-  }
+  Stream<bool> get onConnectivityChange;
 }
