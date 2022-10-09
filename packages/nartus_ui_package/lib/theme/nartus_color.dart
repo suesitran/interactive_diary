d09f4bb36cda1b133e78626d1c7395b31e2ae498
DongNguyenQ <41529808+DongNguyenQ@users.noreply.github.com>
2022-10-09 09:23:05 +0700

    Added color scheme and custom color

--- packages/nartus_ui_package/lib/theme/nartus_color.dart
+++ packages/nartus_ui_package/lib/theme/nartus_color.dart
@@ -15,5 +15,43 @@ const MaterialColor lightColorSwatch = MaterialColor(0xFF779ECB, {
 
 class NartusColor {
   static const Color errorColor = Color(0xFF8B0101);
-  static const Color primary = Color(0xFF779ECB);
+  static const Color primary = Color(0xFF7D54F8);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color primaryContainer = Color(0xFFEFEAFE);
+  static const Color onPrimaryContainer = Color(0xFF1C2025);
+
+  static const Color secondary = Color(0xFF7A7A7A);
+  static const Color onSecondary = Color(0xFFFFFFFF);
+  static const Color secondaryContainer = Color(0xFFECECF1);
+  static const Color onSecondaryContainer = Color(0xFF1C2025);
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color onBackground = Color(0xFF1C2025);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color onSurface = Color(0xFF1C2025);
+
+  static const Gradient gradient = LinearGradient(
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      colors: [
+        Color(0xFFFBF2E3),
+        Color(0xFFEFF2FB),
+      ]);
+  static const Color onGradient = Color(0xFF1C2025);
+
+  static const Color red = Color(0xFFB3261E);
+  static const Color onRed = Color(0xFFFFFFFF);
+  static const Color redContainer = Color(0xFFF6E5E4);
+  static const Color onRedContainer = Color(0xFF1C2025);
+
+  static const Color green = Color(0xFF00B14F);
+  static const Color onGreen = Color(0xFFFFFFFF);
+  static const Color greenContainer = Color(0xFFE0F6EA);
+  static const Color onGreenContainer = Color(0xFF1C2025);
+
+  static const Color orange = Color(0xFFF39C14);
+  static const Color onOrange = Color(0xFF1C2025);
+  static const Color orangeContainer = Color(0xFFFEF3E3);
+  static const Color onOrangeContainer = Color(0xFF1C2025);
+
 }
