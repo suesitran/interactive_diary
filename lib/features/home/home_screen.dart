14253766a23ba3891f0b2e556d046253d8d6eb1e
Suesi Tran <suesitran@gmail.com>
2022-10-12 00:22:52 +1100

    [ID-33] - Add home with scaffold to fix issue

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:interactive_diary/constants/dimens.dart';
@@ -16,122 +17,123 @@ class IDHome extends StatefulWidget {
 
 class _IDHomeState extends State<IDHome> with WidgetsBindingObserver {
   @override
-  Widget build(BuildContext context) =>
-      BlocBuilder<LocationBloc, LocationState>(
-        builder: (BuildContext context, LocationState state) {
-          if (state is LocationReadyState) {
-            return Stack(
-              children: <Widget>[
-                GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(state.currentLocation.latitude,
-                          state.currentLocation.longitude),
-                      zoom: 15),
-                  markers: <Marker>{
-                    Marker(
-                      markerId: const MarkerId('currentLocation'),
-                      position: LatLng(state.currentLocation.latitude,
-                          state.currentLocation.longitude),
-                    )
-                  },
-                ),
-                SafeArea(
-                    child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Card(
-                    margin: const EdgeInsets.only(top: Dimension.spacing16),
-                    shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(Dimension.spacing16)),
-                    elevation: 4.0,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: Dimension.spacing16,
-                          vertical: Dimension.spacing8),
-                      child: Text(
-                        state.dateDisplay,
-                        style: const TextStyle(fontWeight: FontWeight.w600),
+  Widget build(BuildContext context) => Scaffold(
+        body: BlocBuilder<LocationBloc, LocationState>(
+          builder: (BuildContext context, LocationState state) {
+            if (state is LocationReadyState) {
+              return Stack(
+                children: <Widget>[
+                  GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                        target: LatLng(state.currentLocation.latitude,
+                            state.currentLocation.longitude),
+                        zoom: 15),
+                    markers: <Marker>{
+                      Marker(
+                        markerId: const MarkerId('currentLocation'),
+                        position: LatLng(state.currentLocation.latitude,
+                            state.currentLocation.longitude),
+                      )
+                    },
+                  ),
+                  SafeArea(
+                      child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Card(
+                      margin: const EdgeInsets.only(top: Dimension.spacing16),
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(Dimension.spacing16)),
+                      elevation: 4.0,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: Dimension.spacing16,
+                            vertical: Dimension.spacing8),
+                        child: Text(
+                          state.dateDisplay,
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
                       ),
                     ),
-                  ),
-                ))
-              ],
+                  ))
+                ],
+              );
+            }
+
+            if (state is LocationInitial) {
+              context.read<LocationBloc>().add(RequestCurrentLocationEvent());
+            }
+
+            if (state is LocationPermissionDeniedState) {
+              context.showDialogAdaptive(
+                  title: Text(S.of(context).locationPermissionDialogTitle),
+                  content: Text(S.of(context).locationPermissionDialogMessage),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          debugPrint('show dialog');
+                          context
+                              .read<LocationBloc>()
+                              .add(ShowDialogRequestPermissionEvent());
+
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                            S.of(context).locationPermissionDialogAllowButton)),
+                    TextButton(
+                        onPressed: () {
+                          debugPrint('click continue button');
+                          Navigator.of(context).pop();
+                          context
+                              .read<LocationBloc>()
+                              .add(RequestDefaultLocationEvent());
+                        },
+                        child: Text(S
+                            .of(context)
+                            .locationPermissionDialogContinueButton)),
+                  ]);
+            }
+
+            if (state is LocationPermissionDeniedForeverState) {
+              context.showDialogAdaptive(
+                  title: Text(S.of(context).locationPermissionDialogTitle),
+                  content: Text(S.of(context).locationPermissionDialogMessage),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          debugPrint('show dialog');
+                          context
+                              .read<LocationBloc>()
+                              .add(OpenAppSettingsEvent());
+
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(S
+                            .of(context)
+                            .locationPermissionDialogOpenSettingsButton)),
+                    TextButton(
+                        onPressed: () {
+                          debugPrint('click continue button');
+                          Navigator.of(context).pop();
+                          context
+                              .read<LocationBloc>()
+                              .add(RequestDefaultLocationEvent());
+                        },
+                        child: Text(S
+                            .of(context)
+                            .locationPermissionDialogContinueButton)),
+                  ]);
+            }
+
+            if (state is AwaitLocationPermissionFromAppSettingState) {
+              WidgetsBinding.instance.addObserver(this);
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          }
-
-          if (state is LocationInitial) {
-            context.read<LocationBloc>().add(RequestCurrentLocationEvent());
-          }
-
-          if (state is LocationPermissionDeniedState) {
-            context.showDialogAdaptive(
-                title: Text(S.of(context).locationPermissionDialogTitle),
-                content: Text(S.of(context).locationPermissionDialogMessage),
-                actions: <Widget>[
-                  TextButton(
-                      onPressed: () {
-                        debugPrint('show dialog');
-                        context
-                            .read<LocationBloc>()
-                            .add(ShowDialogRequestPermissionEvent());
-
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                          S.of(context).locationPermissionDialogAllowButton)),
-                  TextButton(
-                      onPressed: () {
-                        debugPrint('click continue button');
-                        Navigator.of(context).pop();
-                        context
-                            .read<LocationBloc>()
-                            .add(RequestDefaultLocationEvent());
-                      },
-                      child: Text(S
-                          .of(context)
-                          .locationPermissionDialogContinueButton)),
-                ]);
-          }
-
-          if (state is LocationPermissionDeniedForeverState) {
-            context.showDialogAdaptive(
-                title: Text(S.of(context).locationPermissionDialogTitle),
-                content: Text(S.of(context).locationPermissionDialogMessage),
-                actions: <Widget>[
-                  TextButton(
-                      onPressed: () {
-                        debugPrint('show dialog');
-                        context
-                            .read<LocationBloc>()
-                            .add(OpenAppSettingsEvent());
-
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(S
-                          .of(context)
-                          .locationPermissionDialogOpenSettingsButton)),
-                  TextButton(
-                      onPressed: () {
-                        debugPrint('click continue button');
-                        Navigator.of(context).pop();
-                        context
-                            .read<LocationBloc>()
-                            .add(RequestDefaultLocationEvent());
-                      },
-                      child: Text(S
-                          .of(context)
-                          .locationPermissionDialogContinueButton)),
-                ]);
-          }
-
-          if (state is AwaitLocationPermissionFromAppSettingState) {
-            WidgetsBinding.instance.addObserver(this);
-          }
-
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+          },
+        ),
       );
 
   @override
