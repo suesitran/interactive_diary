26c437484398bbc199140704f4cb4389a519b5bf
lqchuong <lqchuong2402@gmail.com>
2022-10-17 12:58:06 +0700

    update branch

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,13 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:interactive_diary/constants/dimens.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 import 'package:interactive_diary/bloc/location/location_bloc.dart';
 import 'package:interactive_diary/generated/l10n.dart';
-import 'package:nartus_ui_package/widgets/marker.dart';
-import 'dart:ui' as ui;
 
 class IDHome extends StatefulWidget {
   const IDHome({
@@ -19,127 +15,161 @@ class IDHome extends StatefulWidget {
 }
 
 class _IDHomeState extends State<IDHome> with WidgetsBindingObserver {
+  bool isAnimation = false;
+  late Future<List<Marker>> futureListMarker;
+
+  Future<List<Marker>> generateListMarkers(
+      double latitude, double longitude) async {
+    List<Marker> markers = [];
+    final BitmapDescriptor icon = isAnimation == true
+        ? await BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(size: Size(24, 24)),
+            'assets/images/marker_ontap.png')
+        : await BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(size: Size(24, 24)),
+            'assets/images/marker_nonetap.png');
+
+    final Marker marker = Marker(
+        markerId: MarkerId(latitude.toString()),
+        position: LatLng(latitude, longitude),
+        icon: icon,
+        onTap: () {
+          setState(() {
+            isAnimation = !isAnimation;
+          });
+        });
+    markers.add(marker);
+    return markers;
+  }
+
   @override
   Widget build(BuildContext context) =>
       BlocBuilder<LocationBloc, LocationState>(
-          builder: (BuildContext context, LocationState state) {
-        GlobalKey iconKey = GlobalKey();
+        builder: (BuildContext context, LocationState state) {
+          if (state is LocationReadyState) {
+            futureListMarker = generateListMarkers(
+                state.currentLocation.latitude,
+                state.currentLocation.longitude);
 
-        if (state is LocationReadyState) {
-          return Stack(
-            children: <Widget>[
-              GoogleMap(
-                initialCameraPosition: CameraPosition(
-                    target: LatLng(state.currentLocation.latitude,
-                        state.currentLocation.longitude),
-                    zoom: 15),
-                markers: <Marker>{
-                  Marker(
-                      markerId: const MarkerId('currentLocation'),
-                      position: LatLng(state.currentLocation.latitude,
-                          state.currentLocation.longitude),
-                      icon: state.icon)
-                },
-              ),
-              SafeArea(
-                  child: Align(
-                alignment: Alignment.topCenter,
-                child: Card(
-                  margin: const EdgeInsets.only(top: Dimension.spacing16),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(Dimension.spacing16)),
-                  elevation: 4.0,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: Dimension.spacing16,
-                        vertical: Dimension.spacing8),
-                    child: Text(
-                      state.dateDisplay,
-                      style: const TextStyle(fontWeight: FontWeight.w600),
+            return Stack(
+              children: <Widget>[
+                FutureBuilder<List<Marker>>(
+                    future: futureListMarker,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Marker>> snapshot) {
+                      if (snapshot.hasData) {
+                        return GoogleMap(
+                            initialCameraPosition: CameraPosition(
+                                target: LatLng(state.currentLocation.latitude,
+                                    state.currentLocation.longitude),
+                                zoom: 15),
+                            markers: Set<Marker>.of(
+                                snapshot.data as Iterable<Marker>));
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }),
+                SafeArea(
+                    child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Card(
+                    margin: const EdgeInsets.only(top: Dimension.spacing16),
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(Dimension.spacing16)),
+                    elevation: 4.0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: Dimension.spacing16,
+                          vertical: Dimension.spacing8),
+                      child: Text(
+                        state.dateDisplay,
+                        style: const TextStyle(fontWeight: FontWeight.w600),
+                      ),
                     ),
                   ),
-                ),
-              ))
-            ],
+                ))
+              ],
+            );
+          }
+
+          if (state is LocationInitial) {
+            context.read<LocationBloc>().add(RequestCurrentLocationEvent());
+          }
+
+          if (state is LocationPermissionDeniedState) {
+            context.showDialogAdaptive(
+                title: Text(S.of(context).locationPermissionDialogTitle),
+                content: Text(S.of(context).locationPermissionDialogMessage),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        debugPrint('show dialog');
+                        context
+                            .read<LocationBloc>()
+                            .add(ShowDialogRequestPermissionEvent());
+
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                          S.of(context).locationPermissionDialogAllowButton)),
+                  TextButton(
+                      onPressed: () {
+                        debugPrint('click continue button');
+                        Navigator.of(context).pop();
+                        context
+                            .read<LocationBloc>()
+                            .add(RequestDefaultLocationEvent());
+                      },
+                      child: Text(S
+                          .of(context)
+                          .locationPermissionDialogContinueButton)),
+                ]);
+          }
+
+          if (state is LocationPermissionDeniedForeverState) {
+            context.showDialogAdaptive(
+                title: Text(S.of(context).locationPermissionDialogTitle),
+                content: Text(S.of(context).locationPermissionDialogMessage),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        debugPrint('show dialog');
+                        context
+                            .read<LocationBloc>()
+                            .add(OpenAppSettingsEvent());
+
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(S
+                          .of(context)
+                          .locationPermissionDialogOpenSettingsButton)),
+                  TextButton(
+                      onPressed: () {
+                        debugPrint('click continue button');
+                        Navigator.of(context).pop();
+                        context
+                            .read<LocationBloc>()
+                            .add(RequestDefaultLocationEvent());
+                      },
+                      child: Text(S
+                          .of(context)
+                          .locationPermissionDialogContinueButton)),
+                ]);
+          }
+
+          if (state is AwaitLocationPermissionFromAppSettingState) {
+            WidgetsBinding.instance.addObserver(this);
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        }
-
-        if (state is LocationInitial) {
-          context
-              .read<LocationBloc>()
-              .add(RequestCurrentLocationEvent(iconKey));
-        }
-
-        if (state is LocationPermissionDeniedState) {
-          context.showDialogAdaptive(
-              title: Text(S.of(context).locationPermissionDialogTitle),
-              content: Text(S.of(context).locationPermissionDialogMessage),
-              actions: <Widget>[
-                TextButton(
-                    onPressed: () {
-                      debugPrint('show dialog');
-                      context
-                          .read<LocationBloc>()
-                          .add(ShowDialogRequestPermissionEvent());
-
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(
-                        S.of(context).locationPermissionDialogAllowButton)),
-                TextButton(
-                    onPressed: () {
-                      debugPrint('click continue button');
-                      Navigator.of(context).pop();
-                      context
-                          .read<LocationBloc>()
-                          .add(RequestDefaultLocationEvent());
-                    },
-                    child: Text(
-                        S.of(context).locationPermissionDialogContinueButton)),
-              ]);
-        }
-
-        if (state is LocationPermissionDeniedForeverState) {
-          context.showDialogAdaptive(
-              title: Text(S.of(context).locationPermissionDialogTitle),
-              content: Text(S.of(context).locationPermissionDialogMessage),
-              actions: <Widget>[
-                TextButton(
-                    onPressed: () {
-                      debugPrint('show dialog');
-                      context.read<LocationBloc>().add(OpenAppSettingsEvent());
-
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(S
-                        .of(context)
-                        .locationPermissionDialogOpenSettingsButton)),
-                TextButton(
-                    onPressed: () {
-                      debugPrint('click continue button');
-                      Navigator.of(context).pop();
-                      context
-                          .read<LocationBloc>()
-                          .add(RequestDefaultLocationEvent());
-                    },
-                    child: Text(
-                        S.of(context).locationPermissionDialogContinueButton)),
-              ]);
-        }
-
-        if (state is AwaitLocationPermissionFromAppSettingState) {
-          WidgetsBinding.instance.addObserver(this);
-        }
-
-        return Scaffold(
-          body: Center(
-            child: MarkerIcon(
-              globalKeyMyWidget: iconKey,
-              height: 40,
-            ),
-          ),
-        );
-      });
+        },
+      );
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
