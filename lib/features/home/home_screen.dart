50d8e3897469c506458a3be0e49badb8cdd3b395
Suesi Tran <tranhathuy@gmail.com>
2022-10-22 11:13:48 +1100

    implement DateLabelView

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:interactive_diary/constants/dimens.dart';
+import 'package:interactive_diary/features/home/widgets/date_label_view.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 import 'package:interactive_diary/bloc/location/location_bloc.dart';
 import 'package:interactive_diary/generated/l10n.dart';
@@ -76,22 +76,7 @@ class _IDHomeState extends State<IDHome> with WidgetsBindingObserver {
                 SafeArea(
                     child: Align(
                   alignment: Alignment.topCenter,
-                  child: Card(
-                    margin: const EdgeInsets.only(top: Dimension.spacing16),
-                    shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(Dimension.spacing16)),
-                    elevation: 4.0,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: Dimension.spacing16,
-                          vertical: Dimension.spacing8),
-                      child: Text(
-                        state.dateDisplay,
-                        style: const TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
+                  child: DateLabelView(dateLabel: state.dateDisplay),
                 ))
               ],
             );
---
+++ lib/features/home/widgets/date_label_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:interactive_diary/gen/assets.gen.dart';
+import 'package:nartus_ui_package/dimens/dimens.dart';
+
+class DateLabelView extends StatelessWidget {
+  final String dateLabel;
+  const DateLabelView({required this.dateLabel, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 24),
+        child: Card(
+          color: Theme.of(context).backgroundColor,
+          elevation: NartusDimens.elevation4,
+          shape: const RoundedRectangleBorder(
+            borderRadius:
+                BorderRadius.all(Radius.circular(NartusDimens.padding54)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              CalendarLabelView(dateLabel: dateLabel),
+              const ProfileIconView()
+            ],
+          ),
+        ),
+      );
+}
+
+class CalendarLabelView extends StatelessWidget {
+  final String dateLabel;
+  const CalendarLabelView({required this.dateLabel, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                vertical: NartusDimens.padding18,
+                horizontal: NartusDimens.padding20),
+            child: SvgPicture.asset(
+              Assets.images.calendar,
+              width: 20,
+              height: 20,
+            ),
+          ),
+          Text(dateLabel)
+        ],
+      );
+}
+
+class ProfileIconView extends StatelessWidget {
+  const ProfileIconView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(NartusDimens.padding8),
+        child: SvgPicture.asset(
+          Assets.images.anonymous,
+          width: 40,
+          height: 40,
+        ),
+      );
+}
