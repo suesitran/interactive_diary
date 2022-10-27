fd81cb0fe23aa94a254ed571aaef49972d15e679
DongNguyenQ <41529808+DongNguyenQ@users.noreply.github.com>
2022-10-27 21:39:04 +0700

    Merged new update from root repo

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -7,7 +7,7 @@ import 'package:nartus_ui_package/nartus_ui.dart';
 import 'package:interactive_diary/bloc/location/location_bloc.dart';
 import 'package:interactive_diary/generated/l10n.dart';
 
-import '../../gen/assets.gen.dart';
+import 'package:interactive_diary/gen/assets.gen.dart';
 
 class IDHome extends StatefulWidget {
   const IDHome({
