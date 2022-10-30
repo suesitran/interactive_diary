ba2cad9572515b1484f0c2a0d3b2dd8487f1fbbe
suesitran <17781268+suesitran@users.noreply.github.com>
2022-10-31 10:55:33 +1100

    fix text scale factor in cupertino dialog

--- packages/nartus_ui_package/lib/extensions/build_context_extension.dart
+++ packages/nartus_ui_package/lib/extensions/build_context_extension.dart
@@ -9,16 +9,20 @@ extension BuildContextExtension on BuildContext {
       List<Widget>? actions,
       bool barrierDismissible = false}) {
     WidgetsBinding.instance.addPostFrameCallback((Duration timeStamp) {
+      double textScaleFactor = MediaQuery.of(this).textScaleFactor;
       if (defaultTargetPlatform == TargetPlatform.iOS) {
         showCupertinoDialog(
             context: this,
-            builder: (BuildContext context) => CupertinoAlertDialog(
-                  title: title,
-                  content: content,
-                  actions: actions ?? <Widget>[],
-                  insetAnimationCurve: Curves.easeIn,
-                ),
-            barrierDismissible: barrierDismissible);
+            builder: (BuildContext context) => MediaQuery(
+              data: MediaQuery.of(this).copyWith(textScaleFactor: textScaleFactor.clamp(0.8, 1.25)),
+          child: CupertinoAlertDialog(
+            title: title,
+            content: content,
+            actions: actions ?? <Widget>[],
+            insetAnimationCurve: Curves.easeIn,
+          )),
+            barrierDismissible: barrierDismissible
+        );
       } else {
         showDialog(
             context: this,
