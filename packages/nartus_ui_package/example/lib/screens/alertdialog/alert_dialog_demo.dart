977c81ca234e689c9e1f6b610e85b51337e3a49c
Truong Ho Phong <hophongit98@gmail.com>
2022-11-25 16:35:20 +0700

    add case long title, long content into widget catalog

--- packages/nartus_ui_package/example/lib/screens/alertdialog/alert_dialog_demo.dart
+++ packages/nartus_ui_package/example/lib/screens/alertdialog/alert_dialog_demo.dart
@@ -9,124 +9,77 @@ class AlertDialogDemo extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Alert Dialog demo'),
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextButton(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content: 'This is a content',
+                        primaryButtonText: 'Primary button',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        });
+                  },
+                  child: const Text('primary button only')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content:
+                            'This ID bottom sheet has primary and secondary button',
+                        primaryButtonText: 'Primary button',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        secondaryButtonText: 'Secondary',
+                        onSecondaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        });
+                  },
+                  child: const Text('Primary & secondary button')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content:
+                            'This ID bottom sheet has primary and text button',
+                        primaryButtonText: 'Primary button',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        textButtonText: 'Text button',
+                        onTextButtonSelected: () {
+                          Navigator.of(context).pop();
+                        });
+                  },
+                  child: const Text('Primary & Text button')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline very very very very very very very long long long',
+                        content:
+                        'This ID bottom sheet has primary and text button, This ID bottom sheet has primary and text button,, This ID bottom sheet has primary and text button, This ID bottom sheet has primary and text button,',
+                        primaryButtonText: 'Primary button',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        textButtonText: 'Text button',
+                        onTextButtonSelected: () {
+                          Navigator.of(context).pop();
+                        });
+                  },
+                  child: const Text('Primary & Long Title & Long Content')),
+              TextButton(
                 onPressed: () {
                   context.showIDAlertDialog(
                       title: 'Warning Headline',
-                      content: 'This is a content',
+                      content: 'This ID bottom sheet has all 3 buttons',
                       primaryButtonText: 'Primary button',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      });
-                },
-                child: const Text('primary button only')),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content:
-                          'This ID bottom sheet has primary and secondary button',
-                      primaryButtonText: 'Primary button',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      secondaryButtonText: 'Secondary',
-                      onSecondaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      });
-                },
-                child: const Text('Primary & secondary button')),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content:
-                          'This ID bottom sheet has primary and text button',
-                      primaryButtonText: 'Primary button',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      textButtonText: 'Text button',
-                      onTextButtonSelected: () {
-                        Navigator.of(context).pop();
-                      });
-                },
-                child: const Text('Primary & Text button')),
-            TextButton(
-              onPressed: () {
-                context.showIDAlertDialog(
-                    title: 'Warning Headline',
-                    content: 'This ID bottom sheet has all 3 buttons',
-                    primaryButtonText: 'Primary button',
-                    onPrimaryButtonSelected: () {
-                      Navigator.of(context).pop();
-                    },
-                    secondaryButtonText: 'Secondary button',
-                    onSecondaryButtonSelected: () {
-                      Navigator.of(context).pop();
-                    },
-                    textButtonText: 'Text button',
-                    onTextButtonSelected: () {
-                      Navigator.of(context).pop();
-                    });
-              },
-              child: const Text('3 buttons'),
-            ),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content: 'Mi ut pretium, convansi, gravida impresdte',
-                      primaryButtonText: 'Primary',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      iconPath: 'assets/danger.svg');
-                },
-                child: const Text('Primary button With icon')),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content: 'Mi ut pretium, convansi, gravida impresdte',
-                      primaryButtonText: 'Primary',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      secondaryButtonText: 'Secondary button',
-                      onSecondaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      iconPath: 'assets/danger.svg');
-                },
-                child: const Text('Primary & secondary button With icon')),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content: 'Mi ut pretium, convansi, gravida impresdte',
-                      primaryButtonText: 'Primary',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      textButtonText: 'Text button',
-                      onTextButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      iconPath: 'assets/danger.svg');
-                },
-                child: const Text('Primary & Text button With icon')),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content: 'Mi ut pretium, convansi, gravida impresdte',
-                      primaryButtonText: 'Primary',
                       onPrimaryButtonSelected: () {
                         Navigator.of(context).pop();
                       },
@@ -137,33 +90,98 @@ class AlertDialogDemo extends StatelessWidget {
                       textButtonText: 'Text button',
                       onTextButtonSelected: () {
                         Navigator.of(context).pop();
-                      },
-                      iconPath: 'assets/danger.svg');
+                      });
                 },
-                child: const Text('3 buttons With icon')),
-            TextButton(
-                onPressed: () {
-                  context.showIDAlertDialog(
-                      title: 'Warning Headline',
-                      content: 'Mi ut pretium, convansi, gravida impresdte',
-                      primaryButtonText: 'Primary',
-                      onPrimaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      secondaryButtonText: 'Secondary button',
-                      onSecondaryButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      textButtonText: 'Text button',
-                      onTextButtonSelected: () {
-                        Navigator.of(context).pop();
-                      },
-                      isDismissible: false,
-                      iconPath: 'assets/danger.svg');
-                },
-                child: const Text(
-                    'Persistent bottom sheet - 3 buttons With icon')),
-          ],
+                child: const Text('3 buttons'),
+              ),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content: 'Mi ut pretium, convansi, gravida impresdte',
+                        primaryButtonText: 'Primary',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        iconPath: 'assets/danger.svg');
+                  },
+                  child: const Text('Primary button With icon')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content: 'Mi ut pretium, convansi, gravida impresdte',
+                        primaryButtonText: 'Primary',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        secondaryButtonText: 'Secondary button',
+                        onSecondaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        iconPath: 'assets/danger.svg');
+                  },
+                  child: const Text('Primary & secondary button With icon')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content: 'Mi ut pretium, convansi, gravida impresdte',
+                        primaryButtonText: 'Primary',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        textButtonText: 'Text button',
+                        onTextButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        iconPath: 'assets/danger.svg');
+                  },
+                  child: const Text('Primary & Text button With icon')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content: 'Mi ut pretium, convansi, gravida impresdte',
+                        primaryButtonText: 'Primary',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        secondaryButtonText: 'Secondary button',
+                        onSecondaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        textButtonText: 'Text button',
+                        onTextButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        iconPath: 'assets/danger.svg');
+                  },
+                  child: const Text('3 buttons With icon')),
+              TextButton(
+                  onPressed: () {
+                    context.showIDAlertDialog(
+                        title: 'Warning Headline',
+                        content: 'Mi ut pretium, convansi, gravida impresdte',
+                        primaryButtonText: 'Primary',
+                        onPrimaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        secondaryButtonText: 'Secondary button',
+                        onSecondaryButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        textButtonText: 'Text button',
+                        onTextButtonSelected: () {
+                          Navigator.of(context).pop();
+                        },
+                        isDismissible: false,
+                        iconPath: 'assets/danger.svg');
+                  },
+                  child: const Text(
+                      'Persistent bottom sheet - 3 buttons With icon')),
+            ],
+          ),
         ),
       );
 }
