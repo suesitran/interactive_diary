514b237daafd8d837d42c078e3c7e4201d61e2dd
hophongit98 <hophongit98@gmail.com>
2022-12-01 00:07:05 +0700

    add action bar

--- lib/features/writediary/id_write_diary.dart
+++ lib/features/writediary/id_write_diary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nartus_ui_package/nartus_ui.dart';
 
 class IDWriteDiary extends StatefulWidget {
   const IDWriteDiary({super.key});
@@ -14,11 +15,39 @@ class _IDWriteDiaryState extends State<IDWriteDiary> {
       title: 'Add text',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Add text'),
+          title: Text('Add text',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleSmall
+                  ?.copyWith(color: NartusColor.dark)),
+          backgroundColor: NartusColor.background,
+          leading: const BackButton(
+            color: NartusColor.backButtonColor,
+          ),
+          actions: [
+            TextButton(
+                onPressed: null,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 22),
+                  child: Text('Save',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleSmall
+                          ?.copyWith(color: NartusColor.primary)),
+                )),
+          ],
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: Column(children: const [
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: TextField(
+              autofocus: true,
+              showCursor: true,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+            ),
+          )
+        ]),
       ),
     );
   }
