83aae06051227dbdd0fd750cbc253f7c2bd4d949
hophongit98 <hophongit98@gmail.com>
2022-12-04 15:21:17 +0700

    [ID-15] remove material root

--- lib/features/writediary/write_diary_screen.dart
+++ lib/features/writediary/write_diary_screen.dart
@@ -1,17 +1,50 @@
-import 'package:flutter/cupertino.dart';
-import 'package:interactive_diary/features/writediary/id_write_diary.dart';
+import 'package:flutter/material.dart';
+import 'package:interactive_diary/features/writediary/location.dart';
+import 'package:nartus_ui_package/nartus_ui.dart';
+import 'package:interactive_diary/generated/l10n.dart';
 
 class WriteDiaryScreen extends StatelessWidget {
   const WriteDiaryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final double textScaleFactor = MediaQuery
-        .of(context)
-        .textScaleFactor;
+    final double textScaleFactor = MediaQuery.of(context).textScaleFactor;
     return MediaQuery(
-        data: MediaQuery.of(context).copyWith(textScaleFactor: textScaleFactor.clamp(0.8, 1.25)),
-        child: const IDWriteDiary()
+      data: MediaQuery.of(context)
+          .copyWith(textScaleFactor: textScaleFactor.clamp(0.8, 1.25)),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(S.of(context).addText,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleSmall
+                  ?.copyWith(color: NartusColor.dark)),
+          backgroundColor: NartusColor.background,
+          leading: const BackButton(
+            color: NartusColor.backButtonColor,
+          ),
+          actions: <Widget>[
+            NartusButton.text(onPressed: () {}, label: S.of(context).save),
+          ],
+        ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          const LocationView(
+              currentLocation:
+                  'Shop 11, The Strand Arcade, 412-414 George St, Sydney NSW 2000, Australia'),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              autofocus: true,
+              showCursor: true,
+              maxLines: null,
+              decoration: const InputDecoration(border: InputBorder.none),
+              style: Theme.of(context).textTheme.bodyText2,
+              keyboardType: TextInputType.multiline,
+            ),
+          ))
+        ]),
+      ),
     );
   }
 }
