3ba84a4c95fefed56afec5487a2e1ead3a92e1c8
suesitran <17781268+suesitran@users.noreply.github.com>
2022-12-05 21:47:53 +1100

    fix widget test

--- test/features/writediary/write_diary_screen_test.dart
+++ test/features/writediary/write_diary_screen_test.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:interactive_diary/bloc/storage/storage_bloc.dart';
 import 'package:interactive_diary/widgets/location_view.dart';
 import 'package:interactive_diary/features/writediary/write_diary_screen.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:nartus_storage/nartus_storage.dart';
 import 'package:nartus_ui_package/widgets/widgets.dart';
 
 import '../../widget_tester_extension.dart';
+import 'write_diary_screen_test.mocks.dart';
 
+@GenerateMocks([StorageBloc])
 void main() {
+  final MockStorageBloc storageBloc = MockStorageBloc();
+
   testWidgets('verify UI write diary screen',
       (WidgetTester widgetTester) async {
+
     WriteDiaryScreen widget = WriteDiaryScreen(
       latLng: const LatLng(long: 0.0, lat: 0.0),
     );
 
-    await widgetTester.wrapAndPump(widget);
+    when(storageBloc.state).thenAnswer((_) => StorageInitial());
+    when(storageBloc.stream).thenAnswer((_) => Stream<StorageState>.value(StorageInitial()));
 
-    expect(find.byType(SvgPicture), findsOneWidget);
-    expect(find.byType(NartusButton), findsOneWidget);
+    await widgetTester.blocWrapAndPump<StorageBloc>(storageBloc, widget);
+
+    // this is Back button in AppBar
+    expect(find.descendant(of: find.byType(AppBar), matching: find.byType(SvgPicture)), findsOneWidget);
+    expect(find.ancestor(of: find.byType(SvgPicture), matching: find.byType(NartusButton)), findsOneWidget);
+
+    // this is Save button in AppBar
+    expect(find.ancestor(of: find.byType(Text), matching: find.byType(NartusButton)), findsOneWidget);
     expect(find.text('Save'), findsOneWidget);
 
     expect(find.byType(LocationView), findsOneWidget);
