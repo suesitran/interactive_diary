df2444443f8f6101c62e0bffbf412d75dff2c9ac
suesitran <17781268+suesitran@users.noreply.github.com>
2022-12-08 23:44:13 +1100

    avoid menu close multiple times when map move

--- lib/features/home/widgets/google_map.dart
+++ lib/features/home/widgets/google_map.dart
@@ -93,7 +93,6 @@ class _GoogleMapViewState extends State<GoogleMapView>
                   onMapCreated: (GoogleMapController controller) =>
                       _onMapCreated(controller),
                   onCameraMoveStarted: () => _closeMenuIfOpening(),
-                  onCameraMove: (_) => _closeMenuIfOpening(),
                   onTap: (_) => _closeMenuIfOpening(),
                   onLongPress: (_) => _closeMenuIfOpening(),
                   markers: data.data ?? <Marker>{},
@@ -209,10 +208,7 @@ class _GoogleMapViewState extends State<GoogleMapView>
             position: widget.currentLocation,
             icon: BitmapDescriptor.fromBytes(Uint8List.view(pngBytes.buffer)),
             zIndex: 1,
-            onTap: () async {
-              /// Move camera to current marker at center
-              await mapController.animateCamera(CameraUpdate.newLatLng(LatLng(
-                  widget.currentLocation.latitude, widget.currentLocation.longitude)));
+            onTap: () {
               if (_controller.value == 1) {
                 _controller.reverse();
               } else {
