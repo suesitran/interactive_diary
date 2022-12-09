bcb4bc53ca0ec7205cb15e8ca5c9e6b4bf4c6b80
suesitran <17781268+suesitran@users.noreply.github.com>
2022-12-09 18:15:56 +1100

    fix analyze

--- test/features/connectivity/bloc/connection_screen_bloc_test.dart
+++ test/features/connectivity/bloc/connection_screen_bloc_test.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/services.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:interactive_diary/features/connectivity/bloc/connection_screen_bloc.dart';
