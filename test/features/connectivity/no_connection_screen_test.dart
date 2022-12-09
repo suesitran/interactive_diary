25f5a794edcc4286424662e3d552929fc9efe086
suesitran <17781268+suesitran@users.noreply.github.com>
2022-12-09 17:17:41 +1100

    fix test

--- test/features/connectivity/no_connection_screen_test.dart
+++ test/features/connectivity/no_connection_screen_test.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:interactive_diary/bloc/connectivity/connectivity_bloc.dart';
+import 'package:interactive_diary/bloc/no_connection_screen/connection_screen_bloc.dart';
 import 'package:interactive_diary/features/connectivity/no_connection_screen.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../widget_tester_extension.dart';
 
+import 'no_connection_screen_test.mocks.dart';
+
+@GenerateMocks(<Type>[ConnectionScreenBloc])
 void main() {
+  final MockConnectionScreenBloc connectionScreenBloc = MockConnectionScreenBloc();
+
+  setUpAll(() {
+    when(connectionScreenBloc.state).thenAnswer((_) => ConnectionScreenInitial());
+    when(connectionScreenBloc.stream).thenAnswer((_) => Stream<ConnectionScreenState>.value(ConnectionScreenInitial()));
+  });
+
   testWidgets('Verify UI of No connection screen',
       (WidgetTester widgetTester) async {
     const NoConnectionScreen widget = NoConnectionScreen();
 
-    await widgetTester.wrapAndPump(widget);
+    await widgetTester.blocWrapAndPump<ConnectionScreenBloc>(
+      connectionScreenBloc,
+        widget);
 
     // verify screen components
     expect(find.byType(SvgPicture), findsOneWidget);
