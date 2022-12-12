46e27fa5c6e83bc2f5fd33135cd321c6c85cf88d
suesitran <17781268+suesitran@users.noreply.github.com>
2022-12-12 16:05:12 +1100

    [ID-94] - fix text scale clamp for full app (#121)
    * [ID-94] - fix text scale clamp for full app
    
    * fix arb syntax

--- lib/route/map_route.dart
+++ lib/route/map_route.dart
@@ -4,8 +4,7 @@ import 'package:interactive_diary/features/connectivity/no_connection_screen.dar
 import 'package:interactive_diary/features/writediary/write_diary_screen.dart';
 import 'package:nartus_storage/nartus_storage.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
-
-import 'package:interactive_diary/main_app_screen.dart';
+import 'package:interactive_diary/features/home/home_screen.dart';
 
 export 'package:go_router/go_router.dart';
 
@@ -20,7 +19,7 @@ final GoRouter appRoute = GoRouter(
     GoRoute(
       path: idHomeRoute,
       builder: (BuildContext context, GoRouterState state) {
-        return const MainAppScreen();
+        return const IDHome();
       },
     ),
     GoRoute(
