b2419d713b058b880819777323fb7e201c98f9a0
suesitran <17781268+suesitran@users.noreply.github.com>
2023-02-22 02:00:37 +1100

    [ID-87] - Spike to research Firebase remote config (#132)

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:interactive_diary/bloc/app_config/app_config_bloc.dart';
 import 'package:interactive_diary/bloc/connectivity/connectivity_bloc.dart';
 import 'package:interactive_diary/bloc/location/location_bloc.dart';
 import 'package:interactive_diary/features/connectivity/bloc/connection_screen_bloc.dart';
 import 'package:interactive_diary/bloc/storage/storage_bloc.dart';
 import 'package:interactive_diary/route/map_route.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:interactive_diary/bloc/get_contents/get_contents_bloc.dart';
-import 'package:interactive_diary/firebase_options.dart';
 import 'package:interactive_diary/generated/l10n.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
-// ignore_for_file: always_specify_types
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  initializeDateFormatting();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   final GoogleMapsFlutterPlatform mapsImplementation =
       GoogleMapsFlutterPlatform.instance;
@@ -31,6 +23,9 @@ void main() async {
 
   runApp(MultiBlocProvider(
     providers: [
+      BlocProvider<AppConfigBloc>(
+        create: (context) => AppConfigBloc()..add(AppRequestInitialise()),
+      ),
       BlocProvider<LocationBloc>(
         create: (BuildContext context) => LocationBloc(),
       ),
