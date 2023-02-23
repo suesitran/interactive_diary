78b92c601af9017cd6a21647838749510e487f09
suesitran <17781268+suesitran@users.noreply.github.com>
2023-02-23 13:19:42 +1100

    Add remote config package

--- lib/bloc/app_config/app_config_bloc.dart
+++ lib/bloc/app_config/app_config_bloc.dart
@@ -1,16 +1,15 @@
-import 'dart:async';
-
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:interactive_diary/firebase_options.dart';
+import 'package:nartus_remote_config/remote_config_manager.dart';
 
 part 'app_config_event.dart';
 part 'app_config_state.dart';
 
 class AppConfigBloc extends Bloc<AppConfigEvent, AppConfigState> {
-  AppConfigBloc() : super(AppConfigInitial()) {
+  final RemoteConfigManager _remoteConfigManager;
+  AppConfigBloc({RemoteConfigManager? remoteConfigManager}) :
+        _remoteConfigManager = remoteConfigManager ?? RemoteConfigManager(),
+        super(AppConfigInitial()) {
     on<AppRequestInitialise>(_initialise);
   }
 
@@ -18,34 +17,13 @@ class AppConfigBloc extends Bloc<AppConfigEvent, AppConfigState> {
     // init Date formatting
     initializeDateFormatting();
 
-    // init Firebase
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-
-    // init remote config
-    await _initRemoteConfig();
-
-    // TODO add other initialise here
+    // await _remoteConfigManager.init();
 
     // inform UI
     emit(AppConfigInitialised());
   }
 
-  Future<void> _initRemoteConfig() async {
-    final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
-    await remoteConfig.setConfigSettings(RemoteConfigSettings(
-      fetchTimeout: const Duration(minutes: 1),
-      minimumFetchInterval: const Duration(hours: 1),
-    ));
 
-    remoteConfig.addListener(() {
-      // TODO get value from Remote Config and use it here
-      _onDebugOptionChanged(remoteConfig.getBool('debug_options'));
-    });
-
-    await remoteConfig.fetchAndActivate();
-  }
 
   void _onDebugOptionChanged(bool debugOptions) {
     // TODO handle debug option change
