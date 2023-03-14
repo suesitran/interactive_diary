6c58336cced9df55a55b629ba99d91c36984be80
suesitran <17781268+suesitran@users.noreply.github.com>
2023-03-14 19:41:56 +1100

    splash screen android 12

--- android/app/src/main/kotlin/com/nartus/interactive_diary/MainActivity.kt
+++ android/app/src/main/kotlin/com/nartus/interactive_diary/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.nartus.interactive_diary
 
+import android.os.Bundle
+import android.view.Window
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState)
+    }
 }
