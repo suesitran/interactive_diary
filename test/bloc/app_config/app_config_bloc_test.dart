cb4b25c375b7e6f922071d059db9217b25a204f2
suesitran <17781268+suesitran@users.noreply.github.com>
2023-03-15 21:24:25 +1100

    [ID-44] Add test for app_config_bloc

--- test/bloc/app_config/app_config_bloc_test.dart
+++ test/bloc/app_config/app_config_bloc_test.dart
@@ -1,13 +1,15 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:interactive_diary/bloc/app_config/app_config_bloc.dart';
 import 'package:mockito/annotations.dart';
 import 'package:nartus_remote_config/remote_config_manager.dart';
 
 @GenerateMocks([RemoteConfigManager])
 void main() {
-  // blocTest(
-  //   'validate app config initalise',
-  //   build: () => AppConfigBloc(),
-  //   act: (bloc) => bloc.add(AppRequestInitialise()),
-  //   expect: () => [isA<AppConfigInitialised>()],
-  //   verify: (bloc) => verify(manager.init()).called(1),
-  // );
+  blocTest(
+    'validate app config initialise',
+    build: () => AppConfigBloc(),
+    act: (bloc) => bloc.add(AppRequestInitialise()),
+    expect: () => [isA<AppConfigInitialised>()],
+  );
 }
