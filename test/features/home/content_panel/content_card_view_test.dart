4c58550d2761489cce9f5b621139ac386c534a2d
lqchuong <lqchuong2402@gmail.com>
2023-05-08 09:39:46 +0700

    fix run tests

--- test/features/home/content_panel/content_card_view_test.dart
+++ test/features/home/content_panel/content_card_view_test.dart
@@ -20,10 +20,12 @@ void main() {
 
     expect(find.text('displayName'), findsOneWidget);
     expect(find.text('description'), findsOneWidget);
-    expect(find.text('Sep 20, 2022 at 18:20 PM'), findsOneWidget);
+    expect(find.text('20 Sep, 2022 at 18:20 PM'), findsOneWidget);
   });
 
-  testWidgets('given no photos, when show ContentCardView, then do not show any photo', (widgetTester) async {
+  testWidgets(
+      'given no photos, when show ContentCardView, then do not show any photo',
+      (widgetTester) async {
     final Widget widget = ContentCardView(
       displayName: 'displayName',
       photoUrl: 'photoUrl',
@@ -36,7 +38,9 @@ void main() {
     expect(find.byType(ClipRRect), findsNothing);
   });
 
-  testWidgets('given there are text and less than 3 pictures, when show ContentCardView, then show all pictures without extra indicator', (widgetTester) async {
+  testWidgets(
+      'given there are text and less than 3 pictures, when show ContentCardView, then show all pictures without extra indicator',
+      (widgetTester) async {
     final Widget widget = ContentCardView(
       displayName: 'displayName',
       photoUrl: 'photoUrl',
@@ -53,17 +57,15 @@ void main() {
     expect(find.byType(ClipRRect), findsNWidgets(2));
   });
 
-  testWidgets('given there are text and 3 pictures, when show ContentCardView, then show all pictures without extra indicator', (widgetTester) async {
+  testWidgets(
+      'given there are text and 3 pictures, when show ContentCardView, then show all pictures without extra indicator',
+      (widgetTester) async {
     final Widget widget = ContentCardView(
       displayName: 'displayName',
       photoUrl: 'photoUrl',
       dateTime: DateTime(2022, 9, 20, 18, 20),
       text: 'description',
-      images: const [
-        'image1',
-        'image2',
-        'image3'
-      ],
+      images: const ['image1', 'image2', 'image3'],
     );
 
     await mockNetworkImagesFor(() => widgetTester.wrapAndPump(widget));
@@ -71,18 +73,15 @@ void main() {
     expect(find.byType(ClipRRect), findsNWidgets(3));
   });
 
-  testWidgets('given there are text and more than 3 pictures, when show ContentCardView, then show 3 pictures with extra indicator', (widgetTester) async {
+  testWidgets(
+      'given there are text and more than 3 pictures, when show ContentCardView, then show 3 pictures with extra indicator',
+      (widgetTester) async {
     final Widget widget = ContentCardView(
       displayName: 'displayName',
       photoUrl: 'photoUrl',
       dateTime: DateTime(2022, 9, 20, 18, 20),
       text: 'description',
-      images: const [
-        'image1',
-        'image2',
-        'image3',
-        'image4'
-      ],
+      images: const ['image1', 'image2', 'image3', 'image4'],
     );
 
     await mockNetworkImagesFor(() => widgetTester.wrapAndPump(widget));
@@ -91,7 +90,9 @@ void main() {
     expect(find.text('+1'), findsOneWidget);
   });
 
-  testWidgets('given there are no text and less than 4 pictures, when show ContentCardView, then show all pictures without extra indicator', (widgetTester) async {
+  testWidgets(
+      'given there are no text and less than 4 pictures, when show ContentCardView, then show all pictures without extra indicator',
+      (widgetTester) async {
     final Widget widget = SingleChildScrollView(
       child: ContentCardView(
         displayName: 'displayName',
@@ -110,18 +111,15 @@ void main() {
     expect(find.byType(ClipRRect), findsNWidgets(3));
   });
 
-  testWidgets('given there are no text and 4 pictures, when show ContentCardView, then show all pictures without extra indicator', (widgetTester) async {
+  testWidgets(
+      'given there are no text and 4 pictures, when show ContentCardView, then show all pictures without extra indicator',
+      (widgetTester) async {
     final Widget widget = SingleChildScrollView(
       child: ContentCardView(
         displayName: 'displayName',
         photoUrl: 'photoUrl',
         dateTime: DateTime(2022, 9, 20, 18, 20),
-        images: const [
-          'image1',
-          'image2',
-          'image3',
-          'image4'
-        ],
+        images: const ['image1', 'image2', 'image3', 'image4'],
       ),
     );
 
@@ -130,19 +128,15 @@ void main() {
     expect(find.byType(ClipRRect), findsNWidgets(4));
   });
 
-  testWidgets('given there are no text and more than 4 pictures, when show ContentCardView, then show 4 pictures with extra indicator', (widgetTester) async {
+  testWidgets(
+      'given there are no text and more than 4 pictures, when show ContentCardView, then show 4 pictures with extra indicator',
+      (widgetTester) async {
     final Widget widget = SingleChildScrollView(
       child: ContentCardView(
         displayName: 'displayName',
         photoUrl: 'photoUrl',
         dateTime: DateTime(2022, 9, 20, 18, 20),
-        images: const [
-          'image1',
-          'image2',
-          'image3',
-          'image4',
-          'image5'
-        ],
+        images: const ['image1', 'image2', 'image3', 'image4', 'image5'],
       ),
     );
 
