eaec932ee7a63f4fac03493f2f227887184257d1
lqchuong <lqchuong2402@gmail.com>
2023-05-11 11:20:45 +0700

    Fix analyse code

--- lib/features/diary_detail/bloc/diary_detail_cubit.dart
+++ lib/features/diary_detail/bloc/diary_detail_cubit.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_quill/flutter_quill.dart';
 import 'package:interactive_diary/features/home/data/diary_display_content.dart';
 
 part 'diary_detail_state.dart';
