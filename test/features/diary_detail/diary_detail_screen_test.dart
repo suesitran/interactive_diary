40c44eda4d1ecdd3da17288e4937cf403ae67604
lqchuong <lqchuong2402@gmail.com>
2023-05-15 09:38:35 +0700

    fix test coverage

--- test/features/diary_detail/diary_detail_screen_test.dart
+++ test/features/diary_detail/diary_detail_screen_test.dart
@@ -5,6 +5,7 @@ import 'package:interactive_diary/features/diary_detail/diary_detail_screen.dart
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 import 'package:network_image_mock/network_image_mock.dart';
+import 'package:interactive_diary/generated/l10n.dart';
 
 import '../../widget_tester_extension.dart';
 
@@ -28,5 +29,26 @@ void main() {
         findsOneWidget);
 
     expect(find.text('Hoang Nguyen'), findsOneWidget);
+  });
+
+  testWidgets(
+      'given back to previous screen '
+      'when tap on Back button on appbar, '
+      'then Diary Detail Screen will be back to previous screen',
+      (widgetTester) async {
+    const Widget widget = DiaryDetailScreen();
+
+    await mockNetworkImagesFor(() => widgetTester.wrapAndPump(widget));
+
+    final findBackButton = find.descendant(
+        of: find.byType(NartusButton),
+        matching: find.bySemanticsLabel(S.current.back));
+
+    expect(findBackButton, findsOneWidget);
+
+    //test pressed button
+    await widgetTester.tap(findBackButton);
+    await widgetTester.pumpAndSettle();
+    expect(find.byType(DiaryDetailScreen), findsNothing);
   });
 }
