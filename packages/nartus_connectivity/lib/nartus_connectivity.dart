e153e8d88964b26fc3baf5546a3408f930e6585d
suesitran <17781268+suesitran@users.noreply.github.com>
2023-06-10 21:28:01 +1000

    ID-163 check internet connection by pinging google server

--- packages/nartus_connectivity/lib/nartus_connectivity.dart
+++ packages/nartus_connectivity/lib/nartus_connectivity.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:nartus_connectivity/src/connectivity_plus_impl/connectivity_plus_impl.dart';
 
@@ -6,18 +7,45 @@ enum ImplType { connectivityPlus }
 
 class ConnectivityService {
   final ImplType type;
-  ConnectivityService(this.type);
+  ConnectivityService(this.type) {
+    switch (type) {
+      case ImplType.connectivityPlus:
+        _streamSubscription = _connectivityPlusImpl.onConnectivityChange
+            .listen((connected) async {
+          _connectivityWatcher.add(await isConnected);
+        });
+        break;
+    }
+  }
 
   final ConnectivityPlusImpl _connectivityPlusImpl = ConnectivityPlusImpl();
 
-  Future<bool> get isConnected {
+  final StreamController<bool> _connectivityWatcher = StreamController();
+
+  late final StreamSubscription<bool> _streamSubscription;
+
+  Future<bool> get isConnected async {
     if (type == ImplType.connectivityPlus) {
-      return _connectivityPlusImpl.isConnected;
+      bool connected = await _connectivityPlusImpl.isConnected;
+
+      if (connected) {
+        // need to ping google to know if there's really internet connection
+        try {
+          final result = await InternetAddress.lookup('google.com');
+          return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+        } on SocketException catch (_) {}
+      }
+
+      return false;
     }
+
     throw UnimplementedError(
         'Only type ImplType.connectivityPlus is supported.');
   }
 
-  Stream<bool> get onConnectivityChange =>
-      _connectivityPlusImpl.onConnectivityChange;
+  Stream<bool> get onConnectivityChange => _connectivityWatcher.stream;
+
+  Future<void> dispose() async {
+    await _streamSubscription.cancel();
+  }
 }
