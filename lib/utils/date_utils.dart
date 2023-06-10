ea9609da4e39405f04cd72641602ae0f1317f510
Truong Ho Phong <hophongit98@gmail.com>
2023-06-10 22:23:05 +0700

    [ID-125] update constant

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -2,8 +2,8 @@ import 'package:intl/intl.dart';
 
 class IDDateUtils {
 
-  static String PATTERN_DD_MMM_YYYY = 'dd MMM, yyyy';
-  static String PATTERN_HH_MM_A = 'HH:mm a';
+  static const String DDMMMYYYY = 'dd MMM, yyyy';
+  static const String HHMMA = 'HH:mm a';
 
   static String formatDateTime(DateTime dateTime, String pattern) {
     return DateFormat(pattern).format(dateTime);
